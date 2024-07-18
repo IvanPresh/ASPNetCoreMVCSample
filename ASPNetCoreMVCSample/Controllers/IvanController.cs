30e6f3adb02772092b932cd68a45cc63a24b45e4
John Precious Umukoro <mcpreciousshalom@hotmail.com>
2024-07-18 11:29:46 -0700

    Add project files.

---
+++ ASPNetCoreMVCSample/Controllers/IvanController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreMVCSample.Controllers
+{
+    public class IvanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View("~/Views/Ivan/Add.cshtml");
+        }
+    }
+}
---
+++ ASPNetCoreMVCSample/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using ASPNetCoreMVCSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace ASPNetCoreMVCSample.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<ProductModel> products = new List<ProductModel> 
+            {
+               new ProductModel 
+               {
+                  Name = "indomie",
+                  Description="instance noodles",
+                  Price= 234.43m,
+                  Quantity= 10
+               },
+                new ProductModel 
+               {
+                  Name = "popcorn",
+                  Description="creamy flakes",
+                  Price= 200.99m,
+                  Quantity= 4
+               },
+                new ProductModel 
+               {
+                  Name = "milk",
+                  Description="diary new",
+                  Price= 567.10m,
+                  Quantity= 26
+               }
+
+
+
+            };
+            
+            return View(products);
+        }
+    }
+}
